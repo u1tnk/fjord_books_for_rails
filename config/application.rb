36a3652376ec4cc249d33ee37c7de435205befaa
hisaotoriyama <hisao.toriyama@gmail.com>
2020-09-25 21:18:13 +0900

    set_defalt_locale erase_blank_in_ja.yml set_title_in_form

--- config/application.rb
+++ config/application.rb
@@ -21,11 +21,3 @@ module BooksApp
     # the framework and any gems in your application.
   end
 end
-
-module BooksApp
-  class Application < Rails::Application
-    config.load_defaults 6.0
-    config.time_zone = "Tokyo"
-    config.i18n.default_locale = :ja
-  end
-end
