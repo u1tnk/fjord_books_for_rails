5ca060bacddcdb09525f825c03101bc9ef87e076
hisaotoriyama <hisao.toriyama@gmail.com>
2020-10-03 11:29:55 +0900

    ja translation

---
+++ db/migrate/20200930111150_add_user_details_to_users.rb
@@ -0,0 +1,8 @@
+class AddUserDetailsToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :name, :string, null: false, default: ""
+    add_column :users, :zip, :integer
+    add_column :users, :address, :string
+    add_column :users, :profile, :text
+  end
+end
