fef6921401d2f6b99730df5d8dd5abc314425190
hisaotoriyama <hisao.toriyama@gmail.com>
2020-10-04 19:09:39 +0900

    mislaneous correction after merge from origin/chapter4_devise

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,15 +1,9 @@
 # frozen_string_literal: true
 
-<<<<<<< HEAD
 module Users
   class RegistrationsController < Devise::RegistrationsController
     before_action :configure_sign_up_params, only: [:create]
     before_action :configure_account_update_params, only: [:update]
-=======
-class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
->>>>>>> origin/chapter4_devise
 
   # GET /resource/sign_up
   # def new
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 # frozen_string_literal: true
 
 require "test_helper"
-=======
-require 'test_helper'
->>>>>>> origin/chapter4_devise
 
 class UserTest < ActiveSupport::TestCase
   # test "the truth" do
